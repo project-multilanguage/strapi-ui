21986a8931fc769eeff9d0663c0610359511599a
Astawa Putra <astawara29@gmail.com>
2023-10-20 14:05:24 +0800

    add ilustrase in homepage

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,13 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Banner() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center gap-[60px]">
-      <div className="w-[619px] text-center text-blue-900 text-[48px] font-extrabold">Pengetahuan Film dalam Genggaman Anda</div>
-      <div className="w-[470px] text-center text-black text-[24px] font-extrabold">Temukan segala hal film dari A hingga Z dalam genggaman Anda</div>
-      <div className="w-[167px] h-[45px] relative">
-        <div className="w-[167px] h-[45px] left-0 top-0 absolute bg-teal-500 rounded-3xl"></div>
-        <div className="left-[25px] top-[8px] absolute text-center text-white text-[24px] font-medium">Let’s Start</div>
+    <div className="relative">
+      <div className="relative w-full h-screen flex flex-col justify-center items-center gap-[60px] z-10">
+        <div className="w-[619px] text-center text-blue-900 text-[48px] font-extrabold">Pengetahuan Film dalam Genggaman Anda</div>
+        <div className="w-[470px] text-center text-black text-[24px] font-extrabold">Temukan segala hal film dari A hingga Z dalam genggaman Anda</div>
+        <div className="w-[167px] h-[45px] relative">
+          <div className="w-[167px] h-[45px] left-0 top-0 absolute bg-teal-500 rounded-3xl"></div>
+          <div className="left-[25px] top-[8px] absolute text-center text-white text-[24px] font-medium">Let’s Start</div>
+        </div>
+      </div>
+      <div className="absolute top-9 left-8 bottom-9 right-8">
+        <Image src={"/cloud.svg"} alt={"cloud"} width={300} height={300} />
+      </div>
+      <div className="absolute top-8 right-20">
+        <Image src={"/cloud.svg"} alt={"cloud"} width={300} height={300} />
+      </div>
+      <div className="absolute right-5 bottom-1/2 ">
+        <Image src={"/cloud.svg"} alt={"cloud"} width={300} height={300} />
+      </div>
+      <div className="absolute right-5 bottom-0 ">
+        <Image src={"/ilustrasi.svg"} alt={"cloud"} width={300} height={300} />
+      </div>
+      <div className="absolute -left-5 bottom-0 ">
+        <Image src={"/green-cloud.svg"} alt={"cloud"} width={300} height={300} />
       </div>
     </div>
   );
--- components/NavbarHomepage.tsx
+++ components/NavbarHomepage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavbarHomepage() {
   return (
-    <nav className="bg-[#E7F3F8]">
+    <nav className="">
       <div className="px-16 flex justify-between py-3 items-center">
         <h1 className="text-xl font-bold">DarkPedia</h1>
         <ol className="text-xl flex gap-6 font-semibold">
