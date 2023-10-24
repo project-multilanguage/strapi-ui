9d074d0f6d8bdd40a15da7eb5785f624022f1b94
Astawa Putra <astawara29@gmail.com>
2023-10-24 10:50:18 +0800

    add padding in select

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="bg-[#E4F2F7] top-0 sticky z-[9999]">
       <div className="px-16 flex justify-between py-3 items-center">
         <h1 className="text-xl font-bold">DarkPedia</h1>
-        <ol className="text-xl flex gap-6 font-semibold">
+        <ol className="text-xl flex gap-7 font-semibold">
           <li>
             <a href="/">Home</a>
           </li>
@@ -16,7 +16,7 @@ export default function Navbar() {
             <a href="/films">Films</a>
           </li>
           <li>
-            <select name="language" id="lang">
+            <select name="language" id="lang" className="p-1">
               <option value="En">English</option>
               <option value="Zh">Chinese</option>
             </select>
