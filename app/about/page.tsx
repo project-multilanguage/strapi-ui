ae7acd56918578d0ea4e0900af52db04fc1d58fe
Astawa Putra <astawara29@gmail.com>
2023-10-24 08:59:47 +0800

    fix navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
-import NavbarHomepage from "@/components/NavbarHomepage";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <NavbarHomepage />
+      <Navbar />
     </div>
   );
 }
